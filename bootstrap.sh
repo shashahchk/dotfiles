350d1faab3d4160915f73d6f05c3282e4afe5e40
shashahchk <shashaminchok@gmail.com>
2025-08-19 00:29:09 +0800

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,5 @@
+mkdir -p $HOME/.config
+
+ln -s $HOME/dotfiles/config/ghostty              $HOME/.config/ghostty
+ln -s $HOME/dotfiles/config/aerospace $HOME/.config/aerospace
+ln -s $HOME/dotfiles/config/sketchybar $HOME/.config/sketchybar
