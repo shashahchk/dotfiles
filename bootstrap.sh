8052ee1c36b27b1e59b52e858be598b70724a4b8
shashahchk <shashaminchok@gmail.com>
2025-08-19 10:52:29 +0800

    feat: add fish config

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,3 +3,4 @@ mkdir -p $HOME/.config
 ln -s $HOME/dotfiles/config/ghostty              $HOME/.config/ghostty
 ln -s $HOME/dotfiles/config/aerospace $HOME/.config/aerospace
 ln -s $HOME/dotfiles/config/sketchybar $HOME/.config/sketchybar
+ln -s $HOME/dotfiles/config/fish $HOME/.config/fish
