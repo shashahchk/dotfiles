860f063bcb909b51b396ac60c801d0b448110fb5
shashahchk <shashaminchok@gmail.com>
2025-08-21 16:59:39 +0800

    fix: fix glitch in emptty workspace swtich

--- config/sketchybar/plugins/space_windows.sh
+++ config/sketchybar/plugins/space_windows.sh
@@ -6,12 +6,10 @@ echo -e "space windows\n" >> $LOG_FILE
 AEROSPACE_FOCUSED_MONITOR=$(aerospace list-monitors --focused | awk '{print $1}')
 AEROSPACE_EMPTY_WORKSPACES=$(aerospace list-workspaces --monitor focused  --empty | awk '{print $1}')
 
-FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused | awk '{print $1}')
 reload_workspace_icon() {
 	echo "reload workspace icon for workspaces" "$@" >> $LOG_FILE
   LABEL_HIGHLIGHT=true
   ICON_HIGHLIGHT=true
-  display=$AEROSPACE_FOCUSED_MONITOR
   BACKGROUND_BORDER_COLOR=$WHITE
 
   if [ "$@" != "$FOCUSED_WORKSPACE"  ]; then
@@ -20,23 +18,27 @@ reload_workspace_icon() {
     BACKGROUND_BORDER_COLOR=$TRANSPARENT
   fi
 
-  sketchybar --set "space.$@" display=$display icon.highlight=$ICON_HIGHLIGHT background.border_color=$BACKGROUND_BORDER_COLOR label.highlight=$LABEL_HIGHLIGHT
+  sketchybar --set "space.$@" icon.highlight=$ICON_HIGHLIGHT background.border_color=$BACKGROUND_BORDER_COLOR label.highlight=$LABEL_HIGHLIGHT
   apps=$(aerospace list-windows --workspace "$@" | awk -F'|' '{gsub(/^ *| *$/, "", $2); print $2}')
   icon_strip=$(source "$CONFIG_DIR/helpers/get_space_icons.sh")
 
-  if echo "$AEROSPACE_EMPTY_WORKSPACES" | grep -q "$@" && [ "$@" != "$FOCUSED_WORKSPACE" ]; then
-    display=0
-  fi
   echo "space.$@" >> $LOG_FILE
 
   sketchybar --set "space.$@" label="$icon_strip" 
 }
 
+FOCUSED_WORKSPACE=$(aerospace list-workspaces --focused | awk '{print $1}')
 if [ "$SENDER" = "aerospace_workspace_change" ]; then
-echo "focused workspace $FOCUSED_WORKSPACE" >> $LOG_FILE
-echo "previous focused workspace $PREV_FOCUSED_WORKSPACE" >> $LOG_FILE
-    reload_workspace_icon "$PREV_FOCUSED_WORKSPACE" &
-    reload_workspace_icon "$FOCUSED_WORKSPACE" &
+  # set visibility first before highlight + icon update to prevent glitch
+  if echo "$AEROSPACE_EMPTY_WORKSPACES" | grep -q "$PREV_FOCUSED_WORKSPACE"; then
+    sketchybar --set "space.$PREV_FOCUSED_WORKSPACE" display=0
+  fi
+
+  reload_workspace_icon "$PREV_FOCUSED_WORKSPACE" &
+  reload_workspace_icon "$FOCUSED_WORKSPACE" &
+
+  sketchybar --set "space.$FOCUSED_WORKSPACE" display=$AEROSPACE_FOCUSED_MONITOR
+
 fi
 
 if [ "$SENDER" = "space_windows_change" ]; then
