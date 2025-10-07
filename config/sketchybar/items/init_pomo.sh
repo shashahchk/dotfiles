2122fbe0d35c9f05f828e347947d1d09556bebdf
shashahchk <shashaminchok@gmail.com>
2025-10-07 13:16:34 +0800

    refactor

--- config/sketchybar/items/init_pomo.sh
+++ config/sketchybar/items/init_pomo.sh
@@ -3,12 +3,11 @@
 echo "initializing pomo"
 
 sketchybar --add item pomo right \
-	   --set pomo script="${PLUGIN_DIR}/pomo.sh status" \
+	   --set pomo script="${PLUGIN_DIR}/pomo.sh" \
 	    label.color="0xffaaffaa" \
 	   label="start" \
            label.drawing=on \
 	   update_freq=1 \
 	   updates=on \
-	   click_script="${PLUGIN_DIR}/pomo.sh click" \
 	   --subscribe pomo mouse.clicked
            
